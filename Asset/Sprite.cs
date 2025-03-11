485fdaf68657e02dbdcebe97a6075c8451d487f0
mos9527 <greats3an@gmail.com>
2025-03-12 07:58:29 +0800

    Upd

--- Asset/Sprite.cs
+++ Asset/Sprite.cs
@@ -36,6 +36,55 @@ public class Sprite : EditorWindow
                             {
                                 textureSettings.spritePivot = new Vector2(0.5f, 0.5f);
                             }
+
+                            importer.SetTextureSettings(textureSettings);
+                            importer.SaveAndReimport();
+                        }
+                    }
+                }
+                processedTextures++;
+                EditorUtility.DisplayProgressBar("Fixing Textures", $"Processing {path}", (float)processedTextures / totalTextures);
+            }
+        }
+        finally
+        {
+            AssetDatabase.StopAssetEditing();
+        }
+        Debug.Log("Textures Fix Done");
+        EditorUtility.ClearProgressBar();
+    }
+
+    [MenuItem("MaimaiRE/AssetRipper Patch/UI Image Perserve Aspect")]
+    private static void FixUIImages()
+    {
+        // XXX: Not actually based on anything. Though works for Sinmai
+        AssetDatabase.StartAssetEditing();
+        try
+        {
+            string[] guids = AssetDatabase.FindAssets("t:GameObject");
+            int totalTextures = guids.Length;
+            int processedTextures = 0;
+            foreach (string guid in guids)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                var importer = AssetImporter.GetAtPath(path) as TextureImporter;
+
+                if (importer != null && importer.textureType == TextureImporterType.Sprite)
+                {
+                    if (importer.spriteImportMode == SpriteImportMode.Single)
+                    {
+                        Texture2D texture = AssetDatabase.LoadAssetAtPath<Texture2D>(path);
+
+                        if (texture != null)
+                        {
+                            TextureImporterSettings textureSettings = new TextureImporterSettings();
+                            importer.ReadTextureSettings(textureSettings);
+                            textureSettings.spriteMeshType = SpriteMeshType.FullRect;
+                            if (texture.width == texture.height)
+                            {
+                                textureSettings.spritePivot = new Vector2(0.5f, 0.5f);
+                            }
+
                             importer.SetTextureSettings(textureSettings);
                             importer.SaveAndReimport();
                         }
