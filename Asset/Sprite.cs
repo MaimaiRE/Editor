881d35c1a8ddc1186a40fa436693b39c041cbe88
mos9527 <greats3an@gmail.com>
2025-03-11 21:13:28 +0800

    Add sprite fix script

---
+++ Asset/Sprite.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Sprite : EditorWindow
+{
+    [MenuItem("MaimaiRE/AssetRipper Patch/Sprite Patch")]
+    private static void FixTextures()
+    {
+        // https://github.com/AssetRipper/AssetRipper/issues/779
+        // Snippet from a kind stranger on the internet. Thx^^
+        AssetDatabase.StartAssetEditing();
+        try
+        {
+            string[] guids = AssetDatabase.FindAssets("t:Texture2D");
+            int totalTextures = guids.Length;
+            int processedTextures = 0;
+            foreach (string guid in guids)
+            {
+                string path = AssetDatabase.GUIDToAssetPath(guid);
+                var importer = AssetImporter.GetAtPath(path) as TextureImporter;
+
+                if (importer != null && importer.textureType == TextureImporterType.Sprite)
+                {
+                    if (importer.spriteImportMode == SpriteImportMode.Single)
+                    {
+                        Texture2D texture = AssetDatabase.LoadAssetAtPath<Texture2D>(path);
+
+                        if (texture != null)
+                        {
+                            TextureImporterSettings textureSettings = new TextureImporterSettings();
+                            importer.ReadTextureSettings(textureSettings);
+                            textureSettings.spriteMeshType = SpriteMeshType.FullRect;
+                            if (texture.width == texture.height)
+                            {
+                                textureSettings.spritePivot = new Vector2(0.5f, 0.5f);
+                            }
+                            importer.SetTextureSettings(textureSettings);
+                            importer.SaveAndReimport();
+                        }
+                    }
+                }
+                processedTextures++;
+                EditorUtility.DisplayProgressBar("Fixing Textures", $"Processing {path}", (float)processedTextures / totalTextures);
+            }
+        }
+        finally
+        {
+            AssetDatabase.StopAssetEditing();
+        }
+        Debug.Log("Textures Fix Done");
+        EditorUtility.ClearProgressBar();
+    }
+}
